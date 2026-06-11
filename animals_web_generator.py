9ad91b6aeeb5d98964b043050667d4dbf0547447
Ignatello <promashtorg@gmail.com>
2026-06-11 18:00:05 +0200

    Erstelle Karten-Element für jedes Tier

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,6 +11,7 @@ with open("Zootopia_Git/animals_template.html", "r", encoding="utf-8") as fileob
 
 # String with animals-data
 output = ''
+
 for animal in animals_data:
     name = animal.get("name")
     characteristics = animal.get("characteristics", {})
@@ -18,18 +19,22 @@ for animal in animals_data:
     animal_type = characteristics.get("type")
     locations = animal.get("locations", [])
     first_location = locations[0] if locations else None
-
+    output += '<li class="cards__item">\n'
     if name:
-        output += f"Name: {name}\n"
+        output += f"Name: {name}<br/>\n"
     if diet:
-        output += f"Diet: {diet}\n"
+        output += f"Diet: {diet}<br/>\n"
     if first_location:
-        output += f"Location: {first_location}\n"
+        output += f"Location: {first_location}<br/>\n"
     if animal_type:
-        output += f"Type: {animal_type}\n"
-    output += "\n"
+        output += f"Type: {animal_type}<br/>\n"
+    output += '</li>'
+    #output += "<br/>\n"
+
 
 final_html = html_temp.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("Zootopia_Git/animals.html", "w", encoding="utf-8") as output_file:
     output_file.write(final_html)
+
+print(output)
